0ce6ee9dcf75aa2273e647ee7ea8144e59a14998
Pascal Baur <pascal.baur@snowflake.com>
2025-07-18 17:15:13 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -11,28 +11,41 @@ USE SCHEMA TECHUP_HACKATHON_FSI;
 -- =====================================================
 -- Create an internal stage called "reports" for storing report files
 CREATE OR REPLACE STAGE reports
-COMMENT = 'Internal stage for storing report files and documents';CREATE OR REPLACE STAGE reports
     DIRECTORY = ( ENABLE = true ) 
     ENCRYPTION = ( TYPE = 'SNOWFLAKE_SSE' )
     COMMENT = 'Internal stage for storing report files and documents';
 
+-- Create an internal stage called "Annual Filings" for storing annual filing documents
+CREATE OR REPLACE STAGE "Annual Filings"
+    DIRECTORY = ( ENABLE = true ) 
+    ENCRYPTION = ( TYPE = 'SNOWFLAKE_SSE' )
+    COMMENT = 'Internal stage for storing annual filing documents and reports';
+
 -- Show stage details
 DESCRIBE STAGE reports;
+DESCRIBE STAGE "Annual Filings";
 
--- List files in the stage (will be empty initially)
+-- List files in the stages (will be empty initially)
 LIST @reports;
+LIST @"Annual Filings";
 
 -- =====================================================
 -- USAGE EXAMPLES
 -- =====================================================
--- To upload files to this stage, you can use:
+-- To upload files to the reports stage, you can use:
 -- PUT file://path/to/local/file.csv @reports;
 
--- To download files from this stage, you can use:
+-- To upload files to the Annual Filings stage, you can use:
+-- PUT file://path/to/local/annual_report.pdf @"Annual Filings";
+
+-- To download files from stages, you can use:
 -- GET @reports/file.csv file://path/to/local/directory/;
+-- GET @"Annual Filings"/annual_report.pdf file://path/to/local/directory/;
 
 -- To copy data from stage to table:
 -- COPY INTO your_table FROM @reports/file.csv;
+-- COPY INTO your_table FROM @"Annual Filings"/annual_report.csv;
 
--- To remove files from stage:
--- REMOVE @reports/file.csv; 
+-- To remove files from stages:
+-- REMOVE @reports/file.csv;
+-- REMOVE @"Annual Filings"/annual_report.pdf; 
